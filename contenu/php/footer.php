60b02b1f3aa7d102144e8d298242f6343095c258
Venture <tido.squat@gmail.com>
2021-01-31 14:22:00 +0100

    creating footer

---
+++ contenu/php/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div id="damoiseaux-footer">
+        <p>&copy; 2002-2021 www.lewhippet.com, La Romance des Damoiseaux -- Textes, Photos et Vidéos tous droits réservés pour tous pays  </p>
+        <br/>
+        <p>
+            Sabine Bourget, diplomée de l'Ecole Nationale du chien
+        </p>
+        <p>
+            N°Eleveur : 030488 / 51 229 - N° MSA : 190 984 700 322 - N° SIRET : 391 602 406 00020 (53)
+        </p>
+        <p>Elevage de Whippet déclaré aux services vétérinaires et à la préfecture de Corrèze sous le N°20050219</p>
+        <br/>
+    </div>
+    <div id="developer"><p>Site structure et hébergement : Benoit Souillat </p></div>
+</footer>
