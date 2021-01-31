c047c9c3ce61a3e75c894adefaa57a8e47956324
Venture <tido.squat@gmail.com>
2021-01-31 18:07:11 +0100

    github.io

--- contenu/whippet.php
+++ contenu/whippet.php
@@ -9,6 +9,55 @@
         <?php
             include "./php/header.php";
         ?>
+        <main>
+            <section id="le-whippet">
+                <h2>Tout savoir sur Le whippet</h2>
+                <section id="conseil-whippet">
+                    <h6 class="hidden">Nos conseils sur le whippet :</h6>
+                    <ul>
+                        <li><a href="#">Bien accueillir votre chiot</a></li>
+                        <li><a href="#">Education</a></li>
+                        <li><a href="#caractere-whippet">Le caractère du Whippet</a></li>
+                        <li><a href="#">L'histoire du whippet</a></li>
+                        <li><a href="#">Coucou</a></li>
+                    </ul>
+                </section>
+                <section id="histoire-whippet">
+                    <h4>L'histoire du Whippet</h4>
+
+                </section>
+                <section id="caractere-whippet">
+                    <h4>Le caractère du Whippet</h4>
+                    <p>                                                                              
+                        L'élégance, l'intelligence, la résistance ... sont les 3 mots clefs qui caractérisent le whippet.
+                        Ce n'est pas une race naine et l'équilibre de ses proportions morphologiques et son mode de vie font de lui le chien de compagnie par excellence.
+                        Ne vous y trompez pas, malgré une silhouette frêle, il est robuste et fait partie des races rarement représentées dans les salles d'attente des vétérinaires. 
+                        ( d'après les statistique "SCC" c'est le chien qui vit le plus longtemps ).
+                        Attiré par les enfants ... ce chien deviendra la poupée de la famille.
+                    </p>
+                    <p>
+                        Le whippet ne nécessite aucun toilettage et sachez qu'il ne dégage aucune odeur ! On dit qu'il est autonettoyant car il se lèche comme un chat si par hasard il s'est sali durant sa promenade.
+                        Est il réellement un chien ? Certains se posent encore la question car son comportement est incomparable.
+                        Le whippet communique surtout par son regard, la complicité avec lui est unique. Paradoxalement, souvent défini comme le plus félin des chiens il est l'un des plus fidèles compagnons.
+                        Il adore par dessus tout ses proches, surtout ses maîtres et les enfants. De ces derniers il acceptera tout, c'est toujours une grande histoire entre les enfants et les whippets. La complicité qui les rapproche est quelques fois inexplicable et tellement magique.
+                        Rares sont ses aboiements, pourtant le whippet est un excellent gardien même s'il n'est pas pris au  sérieux ... Sa détermination prouve son courage et sa fidélité. 
+                        Doté d'une mémoire exceptionnelle il se souviendra du moindre détail. Il est vif et intelligent. Il suffit de l'observer pour comprendre.
+                    </p>
+                    <p>
+                        Si vous l'offensez, gare à ses caprices vengeurs ! Le whippet n'est cependant pas rancunier et appréciera que vous lui pardonniez ses fautes aussi vite que possible.
+                        Il acceptera vos invités mais ne se laissera pas influencer. Il restera hostile s'il ne connaît pas et bien malin celui qui réussira à l'amadouer. Il jouera adroitement de son regard et sait aussi faire d'incroyables pitreries pour attirer l'attention.
+                        Le whippet aime les louanges, les flatteries et les longues soirées sur le canapé.
+                    </p>
+                    <p>
+                        Le whippet ne supporte pas les cris de colère : il les considère tous contre lui. Très vite il deviendrait malheureux jusqu'à même devenir stressé. Mieux vaut ne pas acquérir un Lévrier whippet si vous ne pouvez  pas lui offrir l'amour et la compréhension.
+                        Si vous avez l'honneur de vivre aux cotés d'un whippet vous aurez  sans doute constaté que votre lévrier est attiré par ses congénères ( de même race ). C'est bien normal, le whippet est encore plus heureux s'il partage sa vie avec un autre whippet.
+                        Le deuxième doit être acquis sans retenu et vous pourrez ainsi constater et comprendre ce qui manquait à votre protégé.
+                        Les whippets se tiendront compagnie et vous offriront deux fois plus de bonheur.
+                        Si le whippet correspond à ce que vous recherchez ou plutôt à ce que vous pouvez lui apporter n'hésitez plus ... VOUS NE CHANGEREZ PLUS
+                    </p>
+                </section>
+            </section>
+        </main>
         
 
         <?php
--- index.php
+++ index.php
@@ -10,15 +10,15 @@
         <header class="d-flex flex-column justify-content-center align-items-center">
             <h1 class="mt-3">La Romance des Damoiseaux</h1>
             <h4 class="mb-4">Le whippet un jour, le whippet toujours </h4>
-            <nav class="navbar d-flex justify-content-around w-75 p-3 m-4 font-weight-bold rounded" style="background-color: black;">
+            <nav class="navbar d-flex justify-content-around w-75 p-3 font-weight-bold rounded" style="background-color: black;">
                 <a href="#">Accueil</a>
-                <a href="#">Sommaire</a>
-                <a href="#">Nos Chiots disponibles</a>
+                <a href="./contenu/sommaire.php">Sommaire</a>
+                <a href="./contenu/nos-bébés.php">Nos Chiots disponibles</a>
                 <a href="./contenu/contact.php">Nous Contacter</a>
             </nav>
     
         </header>
-        <section class="row">
+        <section class="row m-4" id="index">
             <aside class="col">
                 <div class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000" id="option" >
                     <ol class="carousel-indicators carousel-indicators-review">
@@ -28,32 +28,32 @@
                         <li data-target="#option" data-slide-to="3"></li>
                     </ol>
                     <div class="carousel-inner font-weight-bold">
-                        <div class='carousel-item active d-flex flex-column h-75'>
-                            <img src="img/hevidence.jpg" class="w-75 align-self-center">
+                        <div class='carousel-item active d-flex flex-column '>
+                            <img src="/img/hevidence/hevidence.jpg" class="w-75 align-self-center">
                             <div class="carousel-caption carousel-caption-move bg-dark opacity-4 rounded">
-                                <p>Il reste un magnifique mâle disponible sur la portée d'Okkaina & Rock. <br/>
-                                Un joli petit bout plein d'énergie et de caractère.</p>
-                                <a href="#" role="button" class="btn btn-pink">Voir nos Bébés disponibles </a>
+                                <p>Retrouvez nos magnifiques whippets sur ce site internet <br/>
+                                </p>
+                                <a href="/contenu/nos-repro.php" class="btn btn-pink">Voir nos Bébés disponibles </a>
                             </div>
                         </div>
-                        <div class='carousel-item d-flex flex-column h-75'>
-                            <img src="img/articles.jpg" class="w-75 align-self-center">
+                        <div class='carousel-item d-flex flex-column '>
+                            <img src="/img/articles.jpg" class="w-75 align-self-center">
                             <div class="carousel-caption carousel-caption-move bg-dark opacity-4 rounded">
                                 <p>De nombreux articles sont disponibles pour le bien de vos whippets<br/>
                                 N'hésitez pas à nous contacter pour plus d'informations</p>
                                 <a href="./contenu/boutique.php" role="button" class="btn btn-pink">Voir les photos</a>
                             </div>
                         </div>
-                        <div class='carousel-item d-flex flex-column h-75'>
-                            <img src="img/chiot.jpg" class="w-75 align-self-center">
+                        <div class='carousel-item d-flex flex-column '>
+                            <img src="/img/chiot.jpg" class="w-75 align-self-center">
                             <div class="carousel-caption carousel-caption-move bg-dark opacity-4 rounded">
                                 <p>Retrouvez tous les mariages prévus et les chiots disponibles.<br/>
                                 En cliquant sur ce lien </p>
-                                <a href="#" role="button" class="btn btn-pink">Nos Chiots Disponibles</a>
+                                <a href="/contenu/nos-bébés.php" role="button" class="btn btn-pink">Nos Chiots Disponibles</a>
                             </div>
                         </div>
-                        <div class='carousel-item d-flex flex-column h-75'>
-                            <img src="img/hevidence.jpg" class="w-75 align-self-center">
+                        <div class='carousel-item d-flex flex-column '>
+                            <img src="img/whippetbois2.jpg" class="w-75 align-self-center">
                             <div class="carousel-caption carousel-caption-move bg-dark opacity-4 rounded">
                                 <p>Voici toutes nos coordonnées,<br/> 
                                 N'hésitez pas à nous contacter pour plus d'informations.</p>
@@ -74,7 +74,7 @@
             </aside>
         </section>
         <section class='d-flex justify-content-center align-items-center row'>
-            <a href="#" role="button" class='btn btn-lg btn-pink btn-enter'>Entrez dans notre site</a>
+            <a href="./contenu/sommaire.php"class='btn btn-lg btn-pink btn-enter'>Entrez dans notre site</a>
         </section>
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
