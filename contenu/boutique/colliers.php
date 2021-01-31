6dc6c9068c998de2587cbcef096a5d305314650d
Venture <tido.squat@gmail.com>
2021-01-31 15:52:52 +0100

    boutique html and CSS start - link ok

--- contenu/boutique/colliers.php
+++ contenu/boutique/colliers.php
@@ -12,7 +12,28 @@
         <main>
             <section class="section-boutique">
                 <h2>Des colliers pour les whippets</h2>
-                <figure></figure>
+                <div class="fig-container">
+                    <a href="#div-collier">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/strass.jpg" alt="Collier Strass" />
+                            </div>
+                            <figcaption>
+                                <h4>Strass</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#div-collier">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/collier.jpg" alt="Collier Whippet" />
+                            </div>
+                            <figcaption>
+                                <h4>Whippet</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
         </main>
 
         <?php 
--- contenu/boutique/manteaux.php
+++ contenu/boutique/manteaux.php
@@ -12,7 +12,62 @@
         <main>
             <section class="section-boutique">
                 <h2>Des manteaux pour les whippets</h2>
-                <figure></figure>
+                <div class="fig-container">
+                    <a href="#div-manteau">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/elvis.jpg" alt="Manteau Elvis" />
+                            </div>
+                            <figcaption>
+                                <h4>Elvis</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#div-manteau">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/bono.jpg" alt="Manteau Bono" />
+                            </div>
+                            <figcaption>
+                                <h4>Bono</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#div-manteau">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/kevin.jpg" alt="Manteau Kevin" />
+                            </div>
+                            <figcaption>
+                                <h4>Kevin</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <a href="#div-manteau">
+                        <figure>
+                            <div>
+                                <img src="/img/boutique/sting.jpg" alt="Manteau Sting" />
+                            </div>
+                            <figcaption>
+                                <h4>Sting</h4>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
+                <div class="hidden" id="div-manteau">
+                    <div>
+                        <img src="/img/boutique/elvis.jpg" alt="Elvis" />
+                    </div>
+                    <div>
+                        <h5>Elvis</h5>
+                        <p>Prix : 110€</p>
+                        <p>Couleurs Disponibles : </p>
+                        <div id="colour">
+
+                        </div>
+
+                    
+                </div>
         </main>
 
         <?php 
